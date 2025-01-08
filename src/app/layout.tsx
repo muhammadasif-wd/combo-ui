28343f96c8dbf3bc60c5d93b9f45ab56b744517d
muhammadasif-wd <muhammadasif.wd@gmail.com>
2025-01-08 21:53:57 +0600

    Add utility functions, theme provider, and UI components; remove unused SVG files

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
+import Header from "@/components/header/header";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Provider from "./provider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +29,10 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <Provider>
+          <Header />
+          <div className="container mx-auto">{children}</div>
+        </Provider>
       </body>
     </html>
   );
---
+++ src/app/provider.tsx
@@ -0,0 +1,20 @@
+import { ThemeProvider } from "@/components/theme/theme-provider";
+
+const Provider = ({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) => {
+  return (
+    <ThemeProvider
+      attribute="class"
+      defaultTheme="system"
+      enableSystem
+      disableTransitionOnChange
+    >
+      {children}
+    </ThemeProvider>
+  );
+};
+
+export default Provider;
